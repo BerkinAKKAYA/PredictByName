2bbe434c9e9746580d615965e76f085c3716804d
Berkin AKKAYA <berkin_akkaya@hotmail.com>
2020-09-03 13:55:39 +0300

    Better Validation

--- scripts/Validator.js
+++ scripts/Validator.js
@@ -1,29 +1,21 @@
 (function() {
 
     const input = document.querySelector("input[type=text]");
-    input.addEventListener("keyup", Validate);
-
-    function Validate()
-    {
-        let char = input.value.slice(-1);
-
-        switch (char) {
-            case " ": char = "";  break;
-            case "ı": char = "i"; break;
-            case "İ": char = "I"; break;
-            case "ç": char = "c"; break;
-            case "Ç": char = "C"; break;
-            case "ş": char = "s"; break;
-            case "Ş": char = "S"; break;
-            case "ö": char = "o"; break;
-            case "Ö": char = "O"; break;
-            case "ü": char = "u"; break;
-            case "Ü": char = "U"; break;
-            case "ğ": char = "g"; break;
-        }
-
-        input.value = input.value.slice(0, -1);
-        input.value += char;
-    }
+    input.addEventListener("keyup", () => {
+        // Remove Turkish characters & Trim
+        input.value = input.value
+            .replace("ı", "i")
+            .replace("İ", "I")
+            .replace("ç", "c")
+            .replace("Ç", "C")
+            .replace("ş", "s")
+            .replace("Ş", "S")
+            .replace("ö", "o")
+            .replace("Ö", "O")
+            .replace("ü", "u")
+            .replace("Ü", "U")
+            .replace("ğ", "g")
+            .trim();
+    });
 
 })();
